5902770755bb0ec4f1f4947765e38fa3efc01e72
LearsyFuentes <mi.defuentes@iesnervion.es>
2024-10-21 00:12:04 +0200

    Corrigo Ejercicio 4

--- Tema2/src/Tema2While/WhileEjercicio4.java
+++ Tema2/src/Tema2While/WhileEjercicio4.java
@@ -9,65 +9,53 @@ public class WhileEjercicio4
 	public static void main(String[] args) 
 	{
 		
-		//Declaro las variables que voy a usar e inicializo a "0" tres de ellas
-				Scanner escaner = new Scanner(System.in);
-				int num;
-				int suma = 0;
-				int i = 0;
-				int num0 = 0;
-				double media = 0.0;
+			//Declaro las variables que voy a usar e inicializo a "0" tres de ellas
+			Scanner scanner = new Scanner(System.in);
+
+	        int sumaPositivos = 0;
+	        int sumaNegativos = 0;
+	        int contadorNegativos = 0;
+	        int contadorCeros = 0;
+	        int numero = 0;	
 				
+			// Pedir 10 números al usuario
+	        for (int i = 1; i <= 10; i++) 
+	        {
+		    System.out.println("Introduce el número " + i + ":");
+		    int numero1 = scanner.nextInt();
+	        }
+	        
+		    
+			// Clasificar el número
+		    if (numero > 0)
+		    {
+		    sumaPositivos += numero; // Sumar positivos
+		    } 
+		    else if (numero < 0) 
+		    	{
+		           sumaNegativos += numero; // Sumar negativos
+		           contadorNegativos++;     // Contar negativos
+		        } 
+		    else 
+		    	{
+		           contadorCeros++;         // Contar ceros
+		    	}
 				
-				//Pido un numero entero positivo al usuario
-				System.out.print("Introduce un número entero, positivo o negativo o 0: ");
+		           
+		           
+		    // Calcular la media de los números negativos
+		    double mediaNegativos = (contadorNegativos > 0) ? (double) sumaNegativos / contadorNegativos : 0;
+		    
+		 // Mostrar resultados
+		    
+	        System.out.println("Suma de los números positivos: " + sumaPositivos);
+	        
+	        System.out.println("Media de los números negativos: " + mediaNegativos);
+		       
+	        System.out.println("Cantidad de ceros introducidos: " + contadorCeros);
 				
-				// Guardo ese numero en num
-				num = escaner.nextInt();
-				
-				while (i!= 10)
-				{
-					i++;
-					
-					//Pido un numero entero positivo al usuario
-					System.out.print("Introduce un número entero, positivo o negativo o 0: ");
-					
-					// Guardo ese numero en num
-					num = escaner.nextInt();
-					
-					switch (num)
-					{
-					
-						case (num> 0):
-						{
-							
-							suma += num;
-							System.out.print("La suma de los númerros positivos es: " + suma);
-							break;
-						}
-					
-					case (num == 0):
-					{
-						num0 ++;
-						System.out.print("Número de 0 que ha introducido: " + num0);
-						break;
-					}
-					
-					case (num<0):
-					{	
-						media = (double) suma / i;
-						System.out.print("La media de los números negativos es: " + media);
-						break;
-					}
-					
-				  }
-					
-			}
-				
-				
-				
-				
-				//Ciero el escaner
-				escaner.close();
+		   //Cierro el escaner
+		   scanner.close();
 	}
 
 }
